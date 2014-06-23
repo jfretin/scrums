464ccc31514d6b611f8120ea6831929b7d544be4
Fretin Jonathan <fretinjonathan@gmail.com>
2014-06-23 16:29:57 +0200

    first test.

--- scraper.php
+++ scraper.php
@@ -2,16 +2,35 @@
 // This is a template for a PHP scraper on Morph (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
+$html = scraperwiki::scrape("http://www.leboncoin.fr/telephonie/675699535.htm?ca=17_s");
 //
 // // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
+$dom = new simple_html_dom();
+$dom->load($html);
+
+$phoneLink = $("#phoneNumber a");
+if ($phoneLink->href != "") {
+    $jsonImg = scraperwiki::scrape("http://www2.leboncoin.fr/ajapi/get/phone?list_id=675699535");
+    if ($jsonImg != '""') {
+        $i = json_decode($jsonImg);
+        $src = $i->phoneUrl;
+        if ($src != "") {
+            $md5 = md5($src);
+            if (scraperwiki::select("* from data where 'adId'='675699535'")) {
+                echo "675699535 already in DB!\n";
+            } else {
+                $img = base64_encode(file_get_contents($src));
+                scraperwiki::save_sqlite(array('adId'), array('md5' => $md5, 'adId' => "675699535", 'content' => $img));
+                echo "saved 675699535 in DB\n";
+            }
+        }
+    }
+}
+
 //
 // // Write out to the sqlite database using scraperwiki library
 // scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
