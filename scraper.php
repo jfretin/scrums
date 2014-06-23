3e02a0b4859b8d6a0a0b01c81a1584ee93f13ab2
Fretin Jonathan <fretinjonathan@gmail.com>
2014-06-23 16:36:37 +0200

    more debug

--- scraper.php
+++ scraper.php
@@ -13,9 +13,9 @@ $dom = new simple_html_dom();
 $dom->load($html);
 
 $phoneLink = $dom->find("#phoneNumber a", 0);
-var_dump($phoneLink);
 if ($phoneLink->href != "") {
     $jsonImg = scraperwiki::scrape("http://www2.leboncoin.fr/ajapi/get/phone?list_id=675699535");
+    var_dump($jsonImg);
     if ($jsonImg != '""') {
         $i = json_decode($jsonImg);
         $src = $i->phoneUrl;
